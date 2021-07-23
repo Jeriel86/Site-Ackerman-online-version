08324ce6989776441da560ce5bcffd012dfc9204
Jeriel <jerielmbiedou86@gmail.com>
2021-07-23 14:19:47 +0200

    page gewinnspiel

---
+++ gewinnspiel.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+$pagename = "gewinnspiel";
+$pageStyles= "<link rel='stylesheet' href='assets/css/about.css'>"; //balise pour ajouter un fichier CSS propre à cette page
+include "includes/pages/header.php" ?>
+<div>
+    <h3>Gewinnspiel Teilnahmebedingungen</h3>
+    Die nachfolgenden Bedingungen regeln die Teilnahme an dem Gewinnspiel der Generalagentur Sascha Ackermann sowie die Erhebung und Nutzung der von den Teilnehmern erhobenen oder mitgeteilten Daten. Eine Teilnahme am Gewinnspiel ist ausschließlich zu den hier aufgeführten Teilnahmebedingungen möglich. Mit der Teilnahme akzeptiert der Teilnehmer diese Teilnahmebedingungen.
+
+    <p>Veranstalter und Realisator <br>
+        Das Gewinnspiel wird von Generalagentur Sascha Ackermann, Weinstraße Süd 20, 67098 Bad Dürkheim, Telefon: 06322 790 6 790, Email: info@ackermann.vkb.de veranstaltet
+        <br>Sämtliche Fragen, Kommentare oder Beschwerden zum Gewinnspiel sind an den Veranstalter zu richten.</p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Footer -->
+<?php include "traitements/compteur_visites.php" ?>
+<?php include "includes/pages/footer.php" ?>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
+
