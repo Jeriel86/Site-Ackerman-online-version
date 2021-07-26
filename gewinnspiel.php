cf81d9f9c6eda5bb6971edbf80eee0d57c25d179
Hermanno <Hermann18pavel@gmail.com>
2021-07-26 23:12:25 +0100

    UPDATE : completion of the Gewinnspiel page

--- gewinnspiel.php
+++ gewinnspiel.php
@@ -3,13 +3,187 @@ session_start();
 $pagename = "gewinnspiel";
 $pageStyles= "<link rel='stylesheet' href='assets/css/about.css'>"; //balise pour ajouter un fichier CSS propre à cette page
 include "includes/pages/header.php" ?>
-<div>
-    <h3>Gewinnspiel Teilnahmebedingungen</h3>
-    Die nachfolgenden Bedingungen regeln die Teilnahme an dem Gewinnspiel der Generalagentur Sascha Ackermann sowie die Erhebung und Nutzung der von den Teilnehmern erhobenen oder mitgeteilten Daten. Eine Teilnahme am Gewinnspiel ist ausschließlich zu den hier aufgeführten Teilnahmebedingungen möglich. Mit der Teilnahme akzeptiert der Teilnehmer diese Teilnahmebedingungen.
+<div class="container py-4 d-flex justify-content-center" style="font-size:14px">
+    <div class ="col-md-10">
+        <h3>
+            Gewinnspiel Teilnahmebedingungen
+        </h3>
+        <p>
+            Die nachfolgenden Bedingungen regeln die Teilnahme an dem Gewinnspiel der Generalagentur 
+            Sascha Ackermann sowie die Erhebung und Nutzung der von den Teilnehmern erhobenen oder mitgeteilten Daten. Eine Teilnahme am Gewinnspiel ist ausschließlich zu den hier aufgeführten Teilnahmebedingungen möglich. Mit der Teilnahme akzeptiert der Teilnehmer diese Teilnahmebedingungen .
+        </p>
 
-    <p>Veranstalter und Realisator <br>
-        Das Gewinnspiel wird von Generalagentur Sascha Ackermann, Weinstraße Süd 20, 67098 Bad Dürkheim, Telefon: 06322 790 6 790, Email: info@ackermann.vkb.de veranstaltet
-        <br>Sämtliche Fragen, Kommentare oder Beschwerden zum Gewinnspiel sind an den Veranstalter zu richten.</p>
+        <h5><u>
+            Veranstalter und Realisator
+        </u></h5>
+        <p>
+            Das Gewinnspiel wird von Generalagentur Sascha Ackermann, Weinstraße Süd 20, 67098 Bad Dürkheim, Telefon: 06322 790 6 790, Email: info@ackermann.vkb.de veranstaltet.
+        </p>
+        <p>
+            Sämtliche Fragen, Kommentare oder Beschwerden zum Gewinnspiel sind an den Veranstalter zu richten. 
+        </p>
+
+        <h5><u>
+        Teilnahmebedingungen
+        </u></h5>
+        <ol class="px-3">
+            <li class="m-0">
+                    Die Aktion findet statt vom 1.7.2021 bis 1.6.2022. Letzte Teilnahmemöglichkeit ist der 1.6.2022 um 23.59 Uhr. Es gilt das Datum und die Uhrzeit des Eintrags in der Datenbank. 
+            </li><br>
+            <li>
+                Teilnahmeberechtigt sind Personen, die Ihren Wohnsitz in Deutschland haben und das 18. Lebensjahr vollendet haben. Mitarbeiter der GUHL Ikebana GmbH sowie deren Angehörige i.S.v. § 15 AO, Lebenspartner und Mitarbeiter von Kooperationspartnern, verbundenen Unternehmen und beauftragten Agenturen, die mit der Erstellung oder Abwicklung der Aktion beschäftigt sind oder waren, sind von der Teilnahme ausgeschlossen. 
+            </li><br>
+            <li>
+                Teilnehmer der Aktion ist diejenige Person, die die Teilnahmebedingungen erfüllt und deren Account für die Teilnahme am Gewinnspiel verwendet wurde. Zudem müssen die notwendigen Einverständniserklärungen zur Datenspeicherung erteilt werden.
+            </li> <br>
+            <li>
+                Auf dem Instagram unter dem Namen „agentur_ackermann“ und auf der Internetseite www.beamtenversicherung-vkb.de wird das Gewinnspielaufruf stattfinden. Die Teilnahme erfolgt durch Bestätigung des Beratungswunsches durch Agentur Sascha Ackermann.
+            </li><br>
+            <li>
+                Nur durch Dateneintragung und zugehöriger Einwilligungserklärung auf der Internetseite wwww.beamtenevrsicherung-vkb.de stimmt der Teilnehmer zu, dass im Zusammenhang mit dem Gewinnspiel und Generalagentur Sascha Ackermann verbundenen im Instagramkanal veröffentlicht werden dürfen.
+            </li><br>
+            <li>
+                <P>
+                    Unter allen Teilnahmebeiträgen werden folgende Preise per Zufall verlost, Gewinnbekanntgabe Dezember 2021, das Gewinn wird postalisch oder persönlich übergeben.
+                </P>
+                <P>Hauptgewinn: E-Bike</P>
+                <P>
+                    Nebengewinne: Überraschungsgewinn in Wert von je 250 Euro
+                </P> 
+            </li><br>
+            <LI>
+                Die Teilnahme an dem Gewinnspiel erfolgt unentgeltlich. Jeder Teilnehmer darf nur einmal teilnehmen. Mehrfach-Teilnahmen werden nicht berücksichtigt.
+            </LI><br>
+            <LI>
+                Die Gewinnspielteilnahme ist unabhängig von der Beratung und Ergebnis. Der Erwerb eines Versicherungsproduktes oder die Inanspruchnahme einer Dienstleistung erhöhen die Gewinnchancen nicht.
+            </LI><br>
+            <li>
+                Bei einem Verstoß gegen diese Teilnahmebedingungen behält sich Generalagentur Ackermann das Recht vor, Personen vom Gewinnspiel auszuschließen. 
+            </li>
+
+        </ol>
+
+        <h5><u>
+            Gewinnbenachrichtigung, keine Barauszahlungen
+        </u></h5>
+            <ol class="px-3">
+                <li>
+                    Die ausgelosten Gewinner werden per Email oder telefonisch kontaktiert und um Bestätigung der Annahme des Gewinns gebeten. Die Gewinner müssen sich bis zum 1.3.2022 melden und ihre Gewinnannahme bestätigen. Des Weiteren müssen Sie dem Veranstalter ihre vollständigen Vor- und Zunamen sowie Postadresse mitteilen. Bestätigt ein Teilnehmer die Annahme des Gewinns nicht innerhalb der genannten Frist verfällt der Gewinn.
+                </li><br>
+                <li>
+                    Die Gewinnunterlagen werden an die postalische Anschrift der Gewinner verschickt oder persönlich vorbeigebracht. Ist eine Zustellung nicht möglich oder sollten die angegebenen Kontaktmöglichkeiten fehlerhaft sein (z.B. falsche Angaben wie unrichtige Namensangaben, ungültige Emailadressen oder postalische Anschriften), wird der Gewinn an den Veranstalter zurückgeschickt und der Gewinn verfällt. Der Veranstalter ist nicht verpflichtet richtige Adressen auszuforschen. Die Nachteile die sich aus der Angabe fehlerhafter Kontaktdaten ergeben, gehen zu Lasten der Teilnehmer. Bzgl. des Datenschutzrechtes verweisen wir auf den untenstehenden Abschnitt.
+                </li><br>
+                <li>
+                    Eine Auszahlung des Gewinns in bar, in Sachwerten, dessen Tausch oder Übertragung auf andere Personen ist nicht möglich. Ein Teilnehmer kann auf den Gewinn verzichten. In diesem Fall wird der Gewinn erneut verlost. 
+                </li>
+            </ol>
+            <h5><u>
+                Rechte
+            </u></h5>
+            <ol class="px-3">
+                <li>
+                    Der Teilnehmer erklärt sich damit einverstanden, dass von ihm im Rahmen des Gewinnspiels veröffentliche Beiträge (Bildbeschreibungen, Statements, Bewertungen, Kommentare, etc.), ggf. auch in Verbindung mit dem vom Teilnehmer angegebenen Namen, vom Veranstalter in Online- sowie Offlinemedien für die Agentur Ackermann kommerziell (das heißt auch für wirtschaftliche Zwecke und Werbung) genutzt, verbreitet sowie auf sonstige Weise Dritten öffentlich zugänglich gemacht werden. Diese Rechteeinräumung erfolgt unentgeltlich, sowie ohne räumliche, zeitliche oder inhaltliche Beschränkung, schließt das Rechte zur Bearbeitung mit ein und kann vom Veranstalter an verbundene Unternehmen, Tochter- und Ländergesellschaften, Agenturen und sonstige Partner sublizenziert werden.
+                </li><br>
+                <li>
+                    Mit dem Absenden oder Hochladen von Inhalten, insbesondere Beiträgen im Rahmen der Teilnahme an dem Gewinnspiel, erklären die Teilnehmer zudem, dass die von ihnen bereitgestellten Inhalte frei von Rechten Dritter sind, also entweder von ihnen stammen oder sie die Einwilligung der Urheber eingeholt haben.
+                </li><br>
+                <li>
+                    Die Teilnehmer stellen den Veranstalter von allen Ansprüchen Dritter frei, die aufgrund der den Teilnehmern bereitgestellten Inhalte entstanden sind. Sie erklären sich bereit, den Veranstalter in jeder zumutbaren Form bei der Abwehr dieser Ansprüche zu unterstützen. 
+                </li>
+            </ol>
+            <h5><u>
+                Gewährleistungsausschluss
+            </u></h5>
+            <ol>
+                <li>
+                    Der Veranstalter behält sich das Recht vor, das Gewinnspiel zu jedem Zeitpunkt ohne Vorankündigung aus wichtigem Grund abzubrechen. Im Falle eines Abbruchs der Aktion aus wichtigem Grund wird der Veranstalter die Teilnehmer unverzüglich hierüber informieren. Ein Abbruch aus wichtigem Grund kann insbesondere erfolgen, wenn aus technischen Gründen eine ordnungsgemäße Durchführung der Aktion nicht mehr gewährleistet werden kann. Der Veranstalter übernimmt keine Gewähr für entgangene Teilnahmechancen durch technisch bedingte Verbindungsstörungen o.ä.
+                </li> <br>
+                <li>
+                    Die Verfügbarkeit und Funktion des Gewinnspiels auf Instagram kann vom Veranstalter nicht gewährleistet werden. Das Gewinnspiel kann aufgrund von äußeren Umständen und Zwängen beendet oder entfernt werden, ohne dass hieraus Ansprüche der Teilnehmer gegenüber dem Veranstalter entstehen.
+                </li><br>
+                <li>
+                    Der Veranstalter haftet ferner nicht für verloren gegangene, verzögerte, fehlgeleitete, beschädigte oder nicht zugestellte Beiträge/Inhalte, aufgrund von die elektronische Kommunikation störenden technischen Schwierigkeiten oder sonstigen Ursachen, wie sie bei Instagram und Facebook oder einem Internet Provider auftreten können. 
+                </li>
+            </ol>
+            <h5><u>
+                Haftungsausschluss
+            </u></h5>
+            <ol class="px-3">
+                <li>
+                    Für eine Haftung des Veranstalters auf Schadensersatz gelten unbeschadet der sonstigen  gesetzlichen  Anspruchsvoraussetzungen  folgende  Haftungsausschlüsse  und Haftungsbegrenzungen:
+                </li><br>
+                <li>
+                    Der Veranstalter haftet unbeschränkt, soweit die Schadensursache auf Vorsatz oder grober Fahrlässigkeit beruht. Ferner haftet der Veranstalter für die leicht fahrlässige Verletzung von wesentlichen Pflichten, deren Verletzung die Erreichung des Vertragszwecks gefährdet, oder für die Verletzung von Pflichten, deren Erfüllung die ordnungsgemäße Durchführung des Gewinnspiels überhaupt erst ermöglichen und auf deren Einhaltung die Vertragspartner regelmäßig vertrauen. In diesem Fall haftet der Veranstalter jedoch nur für den vorhersehbaren, vertragstypischen Schaden. Der Veranstalter haftet nicht für die leicht fahrlässige Verletzung anderer als der in den vorstehenden Sätzen genannten Pflichten.
+                </li><br>
+                <li>
+                    Die vorstehenden Haftungsbeschränkungen gelten nicht bei Verletzung von Leben, Körper und Gesundheit, für einen Mangel nach Übernahme von Beschaffenheitsgarantien für die Beschaffenheit eines Produktes und bei arglistig verschwiegenen Mängeln. Die Haftung nach dem Produkthaftungsgesetz bleibt unberührt.
+                </li><br>
+                <li>
+                4. Soweit die Haftung des Veranstalters ausgeschlossen oder beschränkt ist, gilt dies auch für die persönliche Haftung von Arbeitnehmern, Vertretern und Erfüllungsgehilfen des Veranstalters. 
+                </li>
+            </ol>
+
+        <H5><u>
+            Datenschutz
+        </u></H5>
+        <ol class="px-3">
+            <li>
+                Der Veranstalter verarbeitet die personenbezogenen Daten der Teilnehmer und Gewinner (Vor- und Zuname, postalische Anschrift, Informationen aus dem Instagram) nur für die Durchführung und Abwicklung des Gewinnspiels und den Versand von Gewinnen. Eine darüberhinausgehende Weitergabe der personenbezogenen Daten an Dritte erfolgt nur, soweit dies für die Durchführung des Gewinnspiels oder die Übermittlung von Produktpaketen erforderlich ist, zum Beispiel im Rahmen der Versendung.
+            </li> <br>
+            <li>
+                Rechtsgrundlage für die Verarbeitung ist Art. 6 Abs. 1 S. 1 lit. a DS-GVO. Die Bereitstellung der Daten ist weder gesetzlich noch vertraglich vorgeschrieben, für die Teilnahme aber erforderlich, bspw. um die Gewinnauslosung zu ermöglichen; des Weiteren kann ohne Kenntnis der Daten keine Zustellung von Gewinnen erfolgen.
+            </li><br>
+            <li>
+                Die personenbezogenen Daten der Teilnehmer werden vier Wochen nach dem Versand der Gewinne gelöscht.
+            </li><br>
+            <li>
+                Auf schriftliche oder in Textform mitgeteilte Anforderung hin, können die Teilnehmer vom Veranstalter jederzeit Auskunft darüber verlangen, welche personenbezogenen Daten bei dem Veranstalter gespeichert sind, können deren Berichtigung oder Löschung verlangen oder ihr Recht auf Einschränkung der Verarbeitung oder Übertragbarkeit der Daten geltend machen. Des Weiteren können die Teilnehmer Ihre Datennutzungseinwilligungen jederzeit widerrufen, ohne dass der Widerruf die Rechtmäßigkeit der bis dahin erfolgten Verarbeitung berührt. Die Teilnehmer haben auch das Recht zur Beschwerde bei einer Aufsichtsbehörde. Es erfolgt kein Profiling.
+            </li> <br>
+            <li>
+                Weitere Informationen entnehmen Sie bitte unseren Datenschutzrichtlinien https://beamtenversicherung-vkb.de/datenschutz.php
+            </li> <br>
+            <li>
+                Sollten Sie Fragen zu unseren Datenschutzrichtlinien haben oder Ihre Rechte ausüben wollen, so kontaktieren Sie uns bitte unter info@ackermann.vkb.de
+            </li>
+            <li>
+                Unser Datenschutzbeauftragter ist erreichbar unter info@ackermann.vkb.de
+            </li>
+        </ol>
+        <h5><u>
+            Hinweise und Bedingungen von Instagram
+        </u></h5>
+        <ol class="px-3">
+            <li>
+                Neben diesen Teilnahmebedingungen wird das Verhältnis zwischen dem Veranstalter, dem Teilnehmer und Instagram durch die Nutzungsbedingungen und die Datenschutzregeln von Instagram bestimmt.
+            </li> <br>
+            <li>
+            Die Teilnehmer können gegenüber Instagram keine Ansprüche geltend machen, die im Zusammenhang mit der Nutzung der Gewinnspielapplikation oder Teilnahme am Gewinnspiel entstehen.
+            </li><br>
+            <li>
+            Die Teilnehmer erkennen an, dass sowohl das Gewinnspiel als auch die Gewinnspielapplikation in keiner Weise von Instagram und Facebook gesponsert, unterstützt oder organisiert wird bzw. in keiner Verbindung zu Instagram und Facebook steht.
+            </li> br
+            <li>
+                Alle Informationen und Daten, die durch die Nutzung der Gewinnspielapplikation oder beim Gewinnspiel durch die Teilnehmer mitgeteilt oder von diesen erhoben werden, werden nur dem Veranstalter und nicht Instagram und Facebook bereitgestellt.
+            </li> <br>
+            <li>
+            Sämtliche Anfragen und Hinweise bezüglich des Gewinnspiels sind an den Veranstalter und nicht an Instagram und Facebook zu richten. 
+            </li>
+        </ol>
+
+        <h5><u>
+        Sonstiges
+        </u></h5>
+        <ol class="px-3">
+            <li>
+                Der Rechtsweg ist ausgeschlossen. Es ist ausschließlich das Recht der Bundesrepublik Deutschland anwendbar. Sollten einzelne dieser Teilnahmebedingungen ungültig sein oder werden, bleibt die Gültigkeit der übrigen Teilnahmebedingungen hiervon unberührt. Diese Teilnahmebedingungen können jederzeit von Agentur Ackermann geändert werden.
+            </li><br>
+            <li>
+                Aufgrund der besseren Lesbarkeit wird in den Teilnahmebedingungen nur die männliche Form verwendet. Die weibliche und diverse Form ist selbstverständlich immer miteingeschlossen.
+            </li>
+        </ol>
+
+    </div>
+
 </div>
 
 
